f68a58c5808580519387f0b4a501f60023798302
Chibuzor Okoroafor <chibuzorokoroafor6@gmail.com>
2021-09-16 19:10:38 +0100

    created header component

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "@fortawesome/fontawesome-free/js/all";
+import headerStyles from "../styles/Header.module.css";
+
+/**
+ * @module Header
+ */
+
+/**
+ * Header
+ * @return {JSX}
+ */
+const Header = () => (
+  <header className={headerStyles.header}>
+    <div>
+      <h1 className={headerStyles.firstHeader}>CV-App</h1>
+      <h2 className={headerStyles.secondHeader}>Create your Resume</h2>
+    </div>
+    <div className={headerStyles.scroll}>
+      <i className="fas fa-scroll" />
+    </div>
+  </header>
+);
+
+export default Header;

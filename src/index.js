d6f84bd1edb14b535d9235a5ed68cffc4e063a2c
Chibuzor Okoroafor <chibuzorokoroafor6@gmail.com>
2021-09-16 19:11:38 +0100

    jsdoc comment

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "./styles/index.css";
 import App from "./App";
+
+/**
+ * @fileoverview Index.js is the root file of this project
+ * @author Frostwalker
+ */
 
 ReactDOM.render(
   <React.StrictMode>

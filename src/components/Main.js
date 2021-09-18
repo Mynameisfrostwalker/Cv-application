400c9cb6dc0f4bc66932c367f5b5a84617723d1b
Chibuzor Okoroafor <chibuzorokoroafor6@gmail.com>
2021-09-18 20:46:59 +0100

    added handle change event handler

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,14 +8,40 @@ import main from "../styles/main.module.css";
 class Main extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      values: {
+        photo: "",
+        title: "Default",
+        firstName: "",
+        lastName: "",
+        num: "",
+        mail: "",
+        address: "",
+      },
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    const { values } = this.state;
+    const newObj = { ...values };
+    const keys = Object.keys(newObj);
+    for (let i = 0; i < keys.length; i += 1) {
+      if (e.target.id === keys[i]) {
+        newObj[keys[i]] = e.target.value;
+      }
+    }
+    this.setState({
+      values: newObj,
+    });
   }
 
   render() {
+    const { values } = this.state;
     return (
       <main className={main.mainstyle}>
         <div>
-          <Personal />
+          <Personal change={this.handleChange} values={values} />
           <Education />
           <Experience />
           <Skills />

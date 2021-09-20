b168847eb68e9d264439ea1b8481cde5051ba520
Chibuzor Okoroafor <chibuzorokoroafor6@gmail.com>
2021-09-20 21:54:59 +0100

    added edudisplay function

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,6 +6,7 @@ import Experience from "./Experience";
 import main from "../styles/main.module.css";
 import PersonalPreview from "./PersonalPreview";
 import Icon from "../assets/icon.jpg";
+import EduPreview from "./EduPreview";
 
 class Main extends React.Component {
   constructor(props) {
@@ -21,12 +22,26 @@ class Main extends React.Component {
         mail: ["", uniqid()],
         address: ["", uniqid()],
       },
+      eduValues: [
+        {
+          type: "education",
+          values: {
+            "School Name": ["", uniqid()],
+            Degree: ["", uniqid()],
+            "Start Date": ["", uniqid()],
+            "End Date": ["", uniqid()],
+          },
+          key: uniqid(),
+        },
+      ],
       personalPreview: false,
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleImg = this.handleImg.bind(this);
     this.edit = this.edit.bind(this);
+    this.eduDisplay = this.eduDisplay.bind(this);
+    this.eduChange = this.eduChange.bind(this);
   }
 
   handleImg(e) {
@@ -83,8 +98,37 @@ class Main extends React.Component {
     });
   }
 
+  eduDisplay(obj, index) {
+    const { eduValues } = this.state;
+    if (obj.type === "education") {
+      return (
+        <Education
+          key={obj.key}
+          change={this.eduChange}
+          datakey={index}
+          values={eduValues[index].values}
+        />
+      );
+    }
+    return <EduPreview key={obj.key} />;
+  }
+
+  eduChange(e) {
+    const { eduValues } = this.state;
+    const newarr = [...eduValues];
+    const id = parseInt(e.target.getAttribute("data-key"), 10);
+    const obj = newarr[id].values;
+    const keys = Object.keys(obj);
+    for (let i = 0; i < keys.length; i += 1) {
+      if (e.target.id === keys[i]) {
+        obj[keys[i]][0] = e.target.value;
+      }
+    }
+    this.setState({ eduValues: newarr });
+  }
+
   render() {
-    const { values, personalPreview, imgsrc } = this.state;
+    const { values, personalPreview, imgsrc, eduValues } = this.state;
     return (
       <main className={main.mainstyle}>
         <div>
@@ -99,7 +143,7 @@ class Main extends React.Component {
           ) : (
             <PersonalPreview values={values} editMode={this.edit} />
           )}
-          <Education />
+          {eduValues.map((obj, index) => this.eduDisplay(obj, index))}
           <Experience />
         </div>
       </main>
